2596e36b08112dcefa48e7d5632c036da54d512d
1MonegroManuel <mvm0033270@est.univalle.edu>
2023-05-10 14:12:48 -0400

    first code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace avance5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git");
+            int nro1,nro2;
+            Console.WriteLine("Ingrese 2 numero");
+            nro1=int.Parse(Console.ReadLine());
+            nro2 = int.Parse(Console.ReadLine());
+            Console.Write(nro1 + nro2);
+        }
+    }
+}
